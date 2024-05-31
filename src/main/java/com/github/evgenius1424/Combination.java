ea7bbe04c5a469df26468061a90a578048225a88
evgenius1424 <evgenius1424@yandex.ru>
2024-06-01 00:04:52 +0200

    chore: simplify

--- src/main/java/com/github/evgenius1424/Combination.java
+++ src/main/java/com/github/evgenius1424/Combination.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashSet;
 
 import static com.github.evgenius1424.Permutations.getPermutationsNoRepetitions;
+import static java.lang.Character.getNumericValue;
 
 public class Combination {
     private final int x, y, z;
@@ -13,80 +14,80 @@ public class Combination {
         this.x = x;
         this.y = y;
         this.z = z;
-        initValues();
-    }
-
-    private void initValues() {
-        getPermutationsNoRepetitions(new int[]{x, y, z}).forEach(permutation -> {
-            int a = permutation.charAt(0);
-            int b = permutation.charAt(1);
-            int c = permutation.charAt(2);
-
-            val(a + b + c);
-            val(a + b - c);
-            val(a + b * c);
-            val((a + b) * c);
-
-            if (canDivide(b, c)) {
-                val(a + b / c);
-            }
-            if (canDivide(a + b, c)) {
-                val((a + b) / c);
-            }
-
-            val(a - b + c);
-            val(a - b - c);
-            val(a - b * c);
-            val((a - b) * c);
-
-            if (canDivide(b, c)) {
-                val(a - b / c);
-            }
-            if (canDivide(a - b, c)) {
-                val((a - b) / c);
-            }
-
-            val(a * b + c);
-            val(a * (b + c));
-
-            val(a * b - c);
-            val(a * (b - c));
-
-            val(a * b * c);
-
-            if (canDivide(a * b, c)) {
-                val(a * b / c);
-            }
-
-            if (canDivide(a, b)) {
-                val(a / b + c);
-            }
-            if (canDivide(a, b + c)) {
-                val(a / (b + c));
-            }
-
-            if (canDivide(a, b)) {
-                val(a / b - c);
-            }
-            if (canDivide(a, b - c)) {
-                val(a / (b - c));
-            }
-
-            if (canDivide(a, b)) {
-                val(a / b * c);
-            }
-            if (canDivide(a, (b * c))) {
-                val(a / (b * c));
-            }
-
-            if (canDivide(a, b) && canDivide(b, c)) {
-                val(a / b / c);
-            }
-        });
+        for (var permutation : getPermutationsNoRepetitions(new int[]{x, y, z})) {
+            int a, b, c;
+            a = getNumericValue(permutation.charAt(0));
+            b = getNumericValue(permutation.charAt(1));
+            c = getNumericValue(permutation.charAt(2));
+            countValues(a, b, c);
+        }
     }
 
     public Collection<Integer> getValues() {
         return values;
+    }
+
+    private void countValues(int a, int b, int c) {
+        val(a + b + c);
+        val(a + b - c);
+        val(a + b * c);
+        val((a + b) * c);
+
+        if (canDivide(b, c)) {
+            val(a + b / c);
+        }
+        if (canDivide(a + b, c)) {
+            val((a + b) / c);
+        }
+
+        val(a - b + c);
+        val(a - b - c);
+        val(a - b * c);
+        val((a - b) * c);
+
+        if (canDivide(b, c)) {
+            val(a - b / c);
+        }
+        if (canDivide(a - b, c)) {
+            val((a - b) / c);
+        }
+
+        val(a * b + c);
+        val(a * (b + c));
+
+        val(a * b - c);
+        val(a * (b - c));
+
+        val(a * b * c);
+
+        if (canDivide(a * b, c)) {
+            val(a * b / c);
+        }
+
+        if (canDivide(a, b)) {
+            val(a / b + c);
+        }
+        if (canDivide(a, b + c)) {
+            val(a / (b + c));
+        }
+
+        if (canDivide(a, b)) {
+            val(a / b - c);
+        }
+        if (canDivide(a, b - c)) {
+            val(a / (b - c));
+        }
+
+        if (canDivide(a, b)) {
+            val(a / b * c);
+        }
+        if (canDivide(a, (b * c))) {
+            val(a / (b * c));
+        }
+
+        if (canDivide(a, b) && canDivide(b, c)) {
+            val(a / b / c);
+        }
     }
 
 
