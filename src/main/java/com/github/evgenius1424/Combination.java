ec8f56556aefc0e0617a3ab090228aae9f180b73
evgenius1424 <evgenius1424@yandex.ru>
2024-06-01 00:02:58 +0200

    chore: simplify

--- src/main/java/com/github/evgenius1424/Combination.java
+++ src/main/java/com/github/evgenius1424/Combination.java
@@ -13,103 +13,89 @@ public class Combination {
         this.x = x;
         this.y = y;
         this.z = z;
-        for (String permutation : getPermutationsNoRepetitions(new int[]{x, y, z})) {
-            int a, b, c;
-            a = Character.getNumericValue(permutation.charAt(0));
-            b = Character.getNumericValue(permutation.charAt(1));
-            c = Character.getNumericValue(permutation.charAt(2));
-            countValues(a, b, c);
-        }
+        initValues();
     }
 
+    private void initValues() {
+        getPermutationsNoRepetitions(new int[]{x, y, z}).forEach(permutation -> {
+            int a = permutation.charAt(0);
+            int b = permutation.charAt(1);
+            int c = permutation.charAt(2);
+
+            val(a + b + c);
+            val(a + b - c);
+            val(a + b * c);
+            val((a + b) * c);
+
+            if (canDivide(b, c)) {
+                val(a + b / c);
+            }
+            if (canDivide(a + b, c)) {
+                val((a + b) / c);
+            }
+
+            val(a - b + c);
+            val(a - b - c);
+            val(a - b * c);
+            val((a - b) * c);
+
+            if (canDivide(b, c)) {
+                val(a - b / c);
+            }
+            if (canDivide(a - b, c)) {
+                val((a - b) / c);
+            }
+
+            val(a * b + c);
+            val(a * (b + c));
+
+            val(a * b - c);
+            val(a * (b - c));
+
+            val(a * b * c);
+
+            if (canDivide(a * b, c)) {
+                val(a * b / c);
+            }
+
+            if (canDivide(a, b)) {
+                val(a / b + c);
+            }
+            if (canDivide(a, b + c)) {
+                val(a / (b + c));
+            }
+
+            if (canDivide(a, b)) {
+                val(a / b - c);
+            }
+            if (canDivide(a, b - c)) {
+                val(a / (b - c));
+            }
+
+            if (canDivide(a, b)) {
+                val(a / b * c);
+            }
+            if (canDivide(a, (b * c))) {
+                val(a / (b * c));
+            }
+
+            if (canDivide(a, b) && canDivide(b, c)) {
+                val(a / b / c);
+            }
+        });
+    }
 
     public Collection<Integer> getValues() {
         return values;
     }
 
-    private void countValues(int a, int b, int c) {
-        //Sum
-        val(a + b + c);
-        val(a + b - c);
-        val(a + b * c);
-        val((a + b) * c);
-
-        if (!isZero(c) && isQuotientInt(b, c)) {
-            val(a + b / c);
-        }
-        if (!isZero(c) && isQuotientInt(a + b, c)) {
-            val((a + b) / c);
-        }
-
-        //Subtraction
-        val(a - b + c);
-        val(a - b - c);
-        val(a - b * c);
-        val((a - b) * c);
-
-        if (!isZero(c) && isQuotientInt(b, c)) {
-            val(a - b / c);
-        }
-        if (!isZero(c) && isQuotientInt(a - b, c)) {
-            val((a - b) / c);
-        }
-
-        //Multiplication
-        val(a * b + c);
-        val(a * (b + c));
-
-        val(a * b - c);
-        val(a * (b - c));
-
-        val(a * b * c);
-
-        if (!isZero(c) && isQuotientInt(a * b, c)) {
-            val(a * b / c);
-        }
-
-        //Division.Sum
-        if (!isZero(b) && isQuotientInt(a, b)) {
-            val(a / b + c);
-        }
-        if (!isZero(b + c) && isQuotientInt(a, b + c)) {
-            val(a / (b + c));
-        }
-
-        //Division.Subtraction
-        if (!isZero(b) && isQuotientInt(a, b)) {
-            val(a / b - c);
-        }
-        if (!isZero(b - c) && isQuotientInt(a, b - c)) {
-            val(a / (b - c));
-        }
-
-        //Division.Multiplication
-        if (!isZero(b) && isQuotientInt(a, b)) {
-            val(a / b * c);
-        }
-        if (!isZero(b * c) && isQuotientInt(a, (b * c))) {
-            val(a / (b * c));
-        }
-
-        //Division.Division
-        if (!isZero(b) && !isZero(c) && isQuotientInt(a, b) && isQuotientInt(b, c)) {
-            val(a / b / c);
-        }
-    }
-
 
     private void val(int value) {
-        if (value > -1) {
-            values.add(value);
-        }
+        if (value > -1) values.add(value);
     }
 
-    private boolean isZero(int x) {
-        return x == 0;
-    }
-
-    private boolean isQuotientInt(int a, int b) {
-        return isZero(a % b);
+    private boolean canDivide(int a, int b) {
+        return b != 0 && a % b == 0;
     }
 
     @Override
