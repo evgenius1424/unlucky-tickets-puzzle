3929eeecfbe1516e5197820414aa5cb50447b328
evgenius1424 <evgenius1424@yandex.ru>
2024-05-31 20:01:23 +0200

    chore: simplify

--- src/main/java/com/github/evgenius1424/Combination.java
+++ src/main/java/com/github/evgenius1424/Combination.java
@@ -7,18 +7,12 @@ import static com.github.evgenius1424.Permutations.getPermutationsNoRepetitions;
 
 public class Combination {
     private final int x, y, z;
-    private final Collection<Integer> values;
+    private final Collection<Integer> values = new HashSet<>();
 
-    Combination(int x, int y, int z) {
+    public Combination(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
-
-        values = new HashSet<>();
-        countValuesForAllPermutations();
-    }
-
-    private void countValuesForAllPermutations() {
         for (String permutation : getPermutationsNoRepetitions(new int[]{x, y, z})) {
             int a, b, c;
             a = Character.getNumericValue(permutation.charAt(0));
@@ -28,78 +22,83 @@ public class Combination {
         }
     }
 
+
+    public Collection<Integer> getValues() {
+        return values;
+    }
+
     private void countValues(int a, int b, int c) {
         //Sum
-        addValue(a + b + c);
-        addValue(a + b - c);
-        addValue(a + b * c);
-        addValue((a + b) * c);
+        val(a + b + c);
+        val(a + b - c);
+        val(a + b * c);
+        val((a + b) * c);
 
         if (!isZero(c) && isQuotientInt(b, c)) {
-            addValue(a + b / c);
+            val(a + b / c);
         }
         if (!isZero(c) && isQuotientInt(a + b, c)) {
-            addValue((a + b) / c);
+            val((a + b) / c);
         }
 
         //Subtraction
-        addValue(a - b + c);
-        addValue(a - b - c);
-        addValue(a - b * c);
-        addValue((a - b) * c);
+        val(a - b + c);
+        val(a - b - c);
+        val(a - b * c);
+        val((a - b) * c);
 
         if (!isZero(c) && isQuotientInt(b, c)) {
-            addValue(a - b / c);
+            val(a - b / c);
         }
         if (!isZero(c) && isQuotientInt(a - b, c)) {
-            addValue((a - b) / c);
+            val((a - b) / c);
         }
 
         //Multiplication
-        addValue(a * b + c);
-        addValue(a * (b + c));
+        val(a * b + c);
+        val(a * (b + c));
 
-        addValue(a * b - c);
-        addValue(a * (b - c));
+        val(a * b - c);
+        val(a * (b - c));
 
-        addValue(a * b * c);
+        val(a * b * c);
 
         if (!isZero(c) && isQuotientInt(a * b, c)) {
-            addValue(a * b / c);
+            val(a * b / c);
         }
 
         //Division.Sum
         if (!isZero(b) && isQuotientInt(a, b)) {
-            addValue(a / b + c);
+            val(a / b + c);
         }
         if (!isZero(b + c) && isQuotientInt(a, b + c)) {
-            addValue(a / (b + c));
+            val(a / (b + c));
         }
 
         //Division.Subtraction
         if (!isZero(b) && isQuotientInt(a, b)) {
-            addValue(a / b - c);
+            val(a / b - c);
         }
         if (!isZero(b - c) && isQuotientInt(a, b - c)) {
-            addValue(a / (b - c));
+            val(a / (b - c));
         }
 
         //Division.Multiplication
         if (!isZero(b) && isQuotientInt(a, b)) {
-            addValue(a / b * c);
+            val(a / b * c);
         }
         if (!isZero(b * c) && isQuotientInt(a, (b * c))) {
-            addValue(a / (b * c));
+            val(a / (b * c));
         }
 
         //Division.Division
         if (!isZero(b) && !isZero(c) && isQuotientInt(a, b) && isQuotientInt(b, c)) {
-            addValue(a / b / c);
+            val(a / b / c);
         }
     }
 
 
-    private void addValue(int value) {
+    private void val(int value) {
         if (value > -1) {
             values.add(value);
         }
@@ -112,11 +111,6 @@ public class Combination {
     private boolean isQuotientInt(int a, int b) {
         return isZero(a % b);
     }
-
-    Collection<Integer> getValues() {
-        return values;
-    }
-
 
     @Override
     public String toString() {
--- src/main/java/com/github/evgenius1424/Main.java
+++ src/main/java/com/github/evgenius1424/Main.java
@@ -3,10 +3,9 @@ package com.github.evgenius1424;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
-import static java.util.Collections.disjoint;
 import static com.github.evgenius1424.Permutations.getPermutationsWithRepetitions;
+import static java.util.Collections.disjoint;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,16 +21,15 @@ public class Main {
                 .map(p -> new Combination(p[0], p[1], p[2]))
                 .toList();
 
-        Set<String> tickets = new HashSet<>();
+        Collection<String> tickets = new HashSet<>();
         for (int i = 0; i < combinations.size(); i++) {
             for (int k = i + 1; k < combinations.size(); k++) {
-                var comb1 = combinations.get(i);
-                var comb2 = combinations.get(k);
-                if (!disjoint(comb1.getValues(), comb2.getValues())) {
-                    continue;
+                var firstCombination = combinations.get(i);
+                var secondCombination = combinations.get(k);
+                if (disjoint(firstCombination.getValues(), secondCombination.getValues())) {
+                    tickets.add(firstCombination + secondCombination.toString());
+                    tickets.add(secondCombination + firstCombination.toString());
                 }
-                tickets.add(comb1 + comb2.toString());
-                tickets.add(comb2 + comb1.toString());
             }
         }
 
